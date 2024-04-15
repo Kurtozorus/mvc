231b6a8a9b47ca60af7f38c4254715979170dec7
Kurtozorus <olivier.marchand26@hotmail.fr>
2024-04-15 15:28:34 +0200

    MVC

---
+++ views/profile.php
@@ -0,0 +1,21 @@
+<?php
+require_once "models/User.php";
+$email = $_POST['email'];
+$user = new User($email);
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+</head>
+<body>
+    <h1>Profile</h1>
+    <p>Bienvenue sur votre page de profile</p>
+    <ul>
+        <li> Email : <?php echo $user->getEmail(); ?></li>
+    </ul>
+    <a href="<?php echo BASE_URL;?> /logout">Se deconnecter</a>
+</body>
+</html>
